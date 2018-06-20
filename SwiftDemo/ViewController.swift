2d1b9e02be017069f70d097d27ebebccf935c200
xiaochen yang <yangxiaochen@chainedbox.com>
2018-06-20 11:27:19 +0800

    first init

---
+++ SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by xiaochen yang on 2018/6/20.
+//  Copyright © 2018年 xiaochen yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
